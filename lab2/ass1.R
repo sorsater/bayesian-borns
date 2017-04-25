7a13cf462dff07f9aa3e01d34825ced5f822424c
Ludvig Noring <ludno249@student.liu.se>
2017-04-25 19:52:35 +0200

    dodgerblue 🐳

--- lab2/ass1.R
+++ lab2/ass1.R
@@ -1,6 +1,9 @@
 require('polynom')
 require('MASS')
 require('geoR')
+set.seed(1234567890)
+imgw = 5
+imgh = 4
 
 data = read.table('TempLinkoping2016.txt', header=TRUE)
 n = nrow(data)
@@ -16,17 +19,57 @@ n = nrow(data)
 #lines(data$time, predict(coefs, data$time))
 
 # Hyper parameters
-mu_0 = c(-10, 40, -40)
+mu_0 = c(-10, 100, -100)
 omega_0 = diag(3)
 v_0 = n - 3
-sigma2_0 = 1
+sigma2_0 = 5
 
 # b
+cols = rainbow(5)
 
-for (i in 1:5){
-  betas = mvrnorm(n = 1, mu = mu_0, Sigma=sigma2_0 * solve(omega_0))
-  sigma2 = rchisq(n = v_0, )
-  
-}
+pdf('plots/hyper.pdf', width=imgw, height=imgh)
+  plot(data, pch=20, cex=0.3, type='n', xlab='Time of year', ylab='Temperature')
+  for (i in 1:5){
+    sigma2 = rinvchisq(1, df=v_0, scale=sigma2_0)
+    betas = mvrnorm(n = 1, mu = mu_0, Sigma=sigma2 * solve(omega_0))
+    coefs = polynomial(betas)
+    lines(data$time, predict(coefs, data$time), col=cols[i])
+  }
+dev.off()
 
+# c
+y = data$temp
+X = as.matrix(data.frame(beta0=rep(1, n), beta1=data$time, beta2=data$time^2))
 
+beta_hat = solve(t(X) %*% X) %*% t(X) %*% y
+mu_n = solve(t(X) %*% X + omega_0) %*% (t(X) %*% X %*% beta_hat + omega_0 %*% mu_0)
+omega_n = t(X) %*% X + omega_0
+v_n = v_0 + n
+v_n.sigma2_n = v_0 * sigma2_0 +
+                (t(y) %*% y +
+                t(mu_0) %*% omega_0 %*% mu_0 -
+                t(mu_n) %*% omega_n %*% mu_n)
+
+sigma2 = rinvchisq(1000, df=v_n, scale=v_n.sigma2_n/v_n) 
+betas = sapply(sigma2, function(sigma2){mvrnorm(n=1, mu=mu_n, Sigma=sigma2*solve(omega_n))})
+betas_mean = apply(betas, 1, mean)
+print(betas_mean)
+
+ci = apply(betas, 1, quantile, probs=c(0.05, 0.95))
+
+cols = c('dodgerblue', 'firebrick', 'forestgreen')
+pdf('plots/hyper.pdf', width=imgw, height=imgh)
+  plot(data, pch=20, cex=0.3, xlab='Time of year', ylab='Temperature')
+  lines(data$time, predict(polynomial(betas_mean), data$time), col=cols[1], lwd=2)
+  lines(data$time, predict(polynomial(ci[1,]), data$time), col=cols[2], lwd=2)
+  lines(data$time, predict(polynomial(ci[2,]), data$time), col=cols[3], lwd=2)
+  legend('topleft', inset=0.02,
+         legend = c('Posterior Mean', '5% interval', '95% interval'),
+         fill = cols)
+dev.off()
+# d
+hot_day = data$time[which.max(predict(polynomial(betas_mean), data$time))]
+hot_day = as.numeric(betas_mean[2] / (-betas_mean[3]*2))
+hot_date = as.Date(hot_day*n, origin="2016-01-01")
+
+# e
--- lab2/ass2.R
+++ lab2/ass2.R
@@ -1,2 +1,57 @@
+require('mvtnorm')
+imgw = 5
+imgh = 4
 data = read.table('WomenWork.dat.txt', header=TRUE)
-data
+response = data$Work
+features = as.matrix(data[,-data$Work])
+
+# a
+fit = glm(Work ~ 0 + ., data = data, family = 'binomial')
+
+# b
+nFeats = ncol(features)
+mu = rep(0, nFeats)
+tau = 10
+sigma2 = tau^2 * diag(nFeats)
+beta_prior = rnorm(mu, sigma2)
+
+logPostLogistic = function(betas, y, X, mu, sigma2) {
+  nPara = length(betas)
+  yPred = as.matrix(X) %*% betas
+  
+  logLike = sum(yPred*y - log(1 + exp(yPred)))
+  if (abs(logLike) == Inf) {
+    logLike = -20000
+  }
+  logPrior = dmvnorm(betas, rep(0, nPara), sigma2, log=TRUE)
+  logPost = logPrior + logLike
+  return (logPost)
+}
+
+initValues = rep(0, nFeats)
+optimResults = optim(initValues,
+                     logPostLogistic, y=response, X=features, mu=mu, sigma2=sigma2,
+                     method=c('BFGS'), control=list(fnscale=-1), hessian=TRUE)
+
+postMode = optimResults$par
+postCov = -solve(optimResults$hessian)
+approxPostStd = sqrt(diag(postCov))
+
+names(postMode) = colnames(features)
+names(approxPostStd) = colnames(features)
+
+childMean = postMode['NSmallChild']
+childStd = approxPostStd['NSmallChild']
+
+betaGrid = seq(-abs(childMean - 4*childStd), abs(childMean + 4* childStd), length = 1000)
+pdf('plots/smallchildren.pdf', width=imgw, height=imgh)
+plot(betaGrid, dnorm(betaGrid, childMean, childStd), 
+     type = "l", lwd = 2, ylab = '', xlab = expression(beta), col='steelblue')
+
+lines(qnorm(c(0.025, 0.975), childMean, childStd), c(0,0), lwd=2, col='tomato')
+dev.off()
+# c
+jane_doe = c(1, 10, 8, 10, 1, 40, 1, 1)
+dmvnorm(jane_doe, mean=postMode, sigma=postCov+diag(8))
+
+
