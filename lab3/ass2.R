6768656e13643cbf7825f0604b351eebe5dcd799
michael <michael.sorsater@gmail.com>
2017-05-11 21:32:00 +0200

    Hopefully a and b done :chart_with_upwards_trend:

--- lab3/ass2.R
+++ lab3/ass2.R
@@ -1,82 +1,60 @@
 require('msm')
+require('mvtnorm')
+
 imgw = 5
 imgh = 4
 data = read.table('WomenWork.dat.txt', header=TRUE)
 Y = data$Work
 X = as.matrix(data[,-data$Work])
-
+nObs = nrow(X)
 
 nFeats = ncol(X)
-mu = 0
+mu_0 = 0
 tau = 10
-beta_prior = rnorm(n=nFeats, mean=mu, sd=tau)
-plot(density(beta_prior))
+mu_0 = rep(0, nFeats)
+omega_0 = tau^2 * diag(nFeats)
+beta_prior = as.matrix(rnorm(n=nFeats, mean=mu_0, sd=sqrt(diag(omega_0))))
 
-
-betaGenerator = function(u, y) {
-  beta_hat = solve(t(u) %*% u) %*% t(u) %*% y
-  return (beta_hat)
-}
-
-uGenerator = function(x, beta, sigma2=1) {
-  mean = t(x)%*%beta
-  sd = sqrt(sigma2)
-  print(mean)
-  draw = rnorm(n=1, mean=mean, sd=sd)
-  
-  return(draw)
-}
-
-#u = 
-#u = c()
-for(i in 1:10) {
-  u = c(u, uGenerator(X, beta))
-  beta = c(beta, betaGenerator(u, Y))
-}
-
-# stopp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# a
-fit = glm(Work ~ 0 + ., data = data, family = 'binomial')
-
-# b
-nFeats = ncol(features)
-mu = rep(0, nFeats)
-tau = 10
-sigma2 = tau^2 * diag(nFeats)
-beta_prior = rnorm(mu, sigma2)
-
-logPostLogistic = function(betas, y, X, mu, sigma2) {
-  nPara = length(betas)
-  yPred = as.matrix(X) %*% betas
-  
-  logLike = sum(yPred*y - log(1 + exp(yPred)))
-  if (abs(logLike) == Inf) {
-    logLike = -20000
+uGenerator = function(betas) {
+  mean = X %*% t(betas)
+  u = rep(0, nObs)
+  for(i in 1:nObs){
+    # Define bounds
+    if (Y[1] == 0){
+      bounds = c(-Inf, 0)
+    }
+    else{
+      bounds = c(0, Inf)
+    }
+    u[i] = rtnorm(n=1, mean=mean[i], sd=1, lower=bounds[1], upper=bounds[2])
   }
-  logPrior = dmvnorm(betas, rep(0, nPara), sigma2, log=TRUE)
-  logPost = logPrior + logLike
-  return (logPost)
+  return(u)
+}
+
+# From the given u, draw new beta-values
+betaGenerator = function(u, sigma2=1) {
+  # From lecture 5, slide 8
+  XtX = t(X) %*% X
+  beta_hat = solve(XtX) %*% t(X) %*% as.matrix(u)
+  mu_n = solve(XtX + omega_0) %*% (XtX %*% beta_hat + omega_0 %*% mu_0)
+  omega_n = XtX + omega_0
   
+  beta = rmvnorm(n=1, mean=mu_n, sigma=(sigma2*solve(omega_n)))
+  return (beta)
+}
+
+# Initial values
+u = uGenerator(t(beta_prior))
+draws = 500
+result_beta = matrix(0, draws, nFeats)
+for(i in 1:draws) {
+  print(i)
+  beta = betaGenerator(u)cd
+  u = uGenerator(beta)
+  result_beta[i,] = beta
+}
+
+# Posterior mean for beta
+res = t(as.matrix(apply(result_beta, 2, mean)))
+colnames(res) = colnames(X)
+res
