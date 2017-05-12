3547ca1baa856a8822fd011936ad006902610c70
Ludvig Noring <ludno249@student.liu.se>
2017-05-12 18:27:07 +0200

    puss👄

--- lab3/ass2.R
+++ lab3/ass2.R
@@ -49,12 +49,64 @@ draws = 500
 result_beta = matrix(0, draws, nFeats)
 for(i in 1:draws) {
   print(i)
-  beta = betaGenerator(u)cd
+  beta = betaGenerator(u)
   u = uGenerator(beta)
   result_beta[i,] = beta
 }
 
+colnames(result_beta) = colnames(X)
 # Posterior mean for beta
 res = t(as.matrix(apply(result_beta, 2, mean)))
-colnames(res) = colnames(X)
 res
+
+
+# c
+nFeats = ncol(X)
+mu = rep(0, nFeats)
+tau = 10
+sigma2 = tau^2 * diag(nFeats)
+beta_prior = rnorm(1, mu, sigma2)
+
+
+logPostProbit <- function(betas, y, X, mu, sigma2) {
+  nPara = length(betas)
+  yPred = as.matrix(X) %*% betas
+  
+  logLike = sum(y*pnorm(yPred, log.p = TRUE) + (1-y)*pnorm(yPred, log.p = TRUE, lower.tail = FALSE))
+  
+  # evaluating the prior
+  logPrior = dmvnorm(betas, matrix(0, nPara, 1), sigma2, log=TRUE);
+  
+  # add the log prior and log-likelihood together to get log posterior
+  return(logLike + logPrior)
+  
+}
+
+initValues = rep(0, nFeats)
+optimResults = optim(initValues,
+                     logPostProbit, y=Y, X=X, mu=mu, sigma2=sigma2,
+                     method=c('BFGS'), control=list(fnscale=-1), hessian=TRUE)
+
+postMode = optimResults$par
+postCov = -solve(optimResults$hessian)
+approxPostStd = sqrt(diag(postCov))
+
+names(postMode) = colnames(X)
+names(approxPostStd) = colnames(X)
+
+
+pdf('plots/betas.pdf')
+par(mfrow=c(4,2))
+for(i in 1:ncol(X)) {
+  betaGrid = seq(min(result_beta[,i]), max(result_beta[,i]), length = 1000)
+  approx_density = dnorm(betaGrid, postMode[i], approxPostStd[i])
+  gibbs_density = hist(result_beta[,i], breaks=30, plot=FALSE)
+  
+  plot(gibbs_density,
+       #xlim=qnorm(c(0.05, 0.95), postMode[i], approxPostStd[i]),
+       ylim=c(0, max(approx_density*sum(gibbs_density$counts), gibbs_density$counts)),
+       xlab=colnames(X)[i], col='dodgerblue'
+       )
+  lines(betaGrid, approx_density*sum(gibbs_density$counts), col='tomato')
+}
+dev.off()
