2a1f19af937a3dbc2ebebd61b7df3ea708370005
michael <michael.sorsater@gmail.com>
2017-05-15 13:24:23 +0200

    Small changes

--- lab4/ass1.R
+++ lab4/ass1.R
@@ -1,8 +1,20 @@
+require('MASS')
 
 ebay = read.table('eBayNumberOfBidderData.dat', header = TRUE)
+X = as.matrix(ebay[,-1])
+Y = ebay[,1]
 
+nFeats = ncol(X)
+nObs = nrow(X)
+
+# a
 glmModel = glm(nBids ~ 0 + ., data=ebay, family=poisson)
-glmModel
+coefs = glmModel$coefficients
+coefs
 
-# Significant:
-# verify, sealed, majblem, minbidsshare
+# b
+sigma2_0 = 100 * solve(t(X) %*% X)
+mu_0 = rep(0, nFeats)
+
+beta_prior = mvrnorm(n=1, mu=mu_0, Sigma=sigma2_0)
+beta_prior
