58e19e936a992a18bd488bf6ae357954206409ad
michael <michael.sorsater@gmail.com>
2017-05-16 20:46:47 +0200

    Show barplot for dpois instead :bar_chart:

--- lab4/ass1.R
+++ lab4/ass1.R
@@ -20,18 +20,16 @@ mu_0 = rep(0, nFeats)
 
 beta_prior = mvrnorm(n=1, mu=mu_0, Sigma=sigma2_0)
 
-
+# Calculate the posterior for the poisson model
 logPostPoi = function(betas, y, X) {
   yPred = as.matrix(X) %*% betas
-  
   lambda = exp(yPred)
-  #logLike = sum(log(exp(-lambda) * lambda^y / factorial(y)))
+  
   logLike = sum(log(exp(-lambda) * lambda^y / factorial(y)))
   logPrior = dmvnorm(betas, mu_0, sigma2_0, log=TRUE);
   
   # add the log prior and log-likelihood together to get log posterior
   return(logLike + logPrior)
-  
 }
 
 initValues = rep(0, nFeats)
@@ -45,20 +43,18 @@ approxPostStd = sqrt(diag(postCov)/nFeats)
 postMode
 
 # c
-
-
 metroDraw = function(logPostFunc, thetas, sigma, c_tilde, ...) {
-  draws = 2000
+  draws = 1000
   result_theta = matrix(0, draws, nFeats)
   mean_conv = matrix(0, draws, nFeats)
   thetas_c = thetas
-  #prob = c()
+  prob = c()
   for(i in 1:draws){
     thetas_p = (mvrnorm(1, thetas, c_tilde*sigma))
     alpha = min(1, exp(logPostFunc(thetas_p, ...) - logPostFunc(thetas_c, ...)))
-    
-    update = sample(c(FALSE, TRUE), 1, prob=c(1-alpha, alpha))
-    #prob = c(prob, alpha)
+    prob = c(prob, alpha)
+    # Update with probability alpha
+    update = sample(c(TRUE, FALSE), 1, prob=c(alpha, 1-alpha))
     if(update)
       thetas_c = thetas_p
     
@@ -66,29 +62,35 @@ metroDraw = function(logPostFunc, thetas, sigma, c_tilde, ...) {
     
     if (i > 1)
       mean_conv[i,] = colMeans(result_theta[1:i,])
-    
-      
   }
-  #print(mean(prob))
+  print(paste('Mean of alpha:', mean(prob)))
   return (mean_conv)
 }
 
-
-c_tilde = 0.5
+c_tilde = 1.2
 thetas = metroDraw(logPostPoi, thetas=postMode, sigma=postCov, c_tilde=c_tilde, y=Y, X=X)
-#plot(thetas[,1], type='l')
-par(mfrow=c(3,3))
-for(i in 1:nFeats) {
-  plot(thetas[,i], type='l', col=rainbow(nFeats)[i], xlab='Samples', ylab=colnames(X)[i])
-}
+
+pdf('plots/convergence.pdf')
+  par(mfrow=c(3,3))
+  for(i in 1:nFeats) {
+    plot(thetas[,i], type='l', col=rainbow(nFeats)[i], xlab='Samples', ylab=colnames(X)[i])
+  }
+dev.off()
 
 # d
 optTheta = thetas[nrow(thetas),]
 femkrona_1972 = c(1, 1, 1, 1, 0, 0, 0, 1, 0.5)
 names(femkrona_1972) = colnames(X)
 lambda = exp(femkrona_1972%*%optTheta)
+
 xGrid = 0:5
-plot(xGrid, ppois(xGrid, lambda), cex=1, pch=20, xlab='k', ylab='P(x<=k)', col='dodgerblue')
-text(0.2, 0.4, paste(100*round(ppois(0, lambda), 2), '%'), col='tomato')
+xAxis = paste(xGrid, ': ', round(100*dpois(xGrid, lambda), 1), '%', sep='')
+xAxis = paste(round(100*dpois(xGrid, lambda), 1), '%', sep='')
 
+#plot(xGrid, dpois(xGrid, lambda), cex=1, pch=20, xlab='k', ylab='P(x=k)', col='dodgerblue', xaxt='n')
+#axis(side=1, at =c(xGrid), labels = xAxis)
 
+pdf('plots/femkrona.pdf')
+  barloc = barplot(dpois(xGrid, lambda), xaxt='n', col='dodgerblue', xlab='Number of bids + probability', ylab='Probability')
+  axis(side=1, at=barloc, labels = xAxis)
+dev.off()
